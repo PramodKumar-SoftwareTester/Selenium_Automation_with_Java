64ff8015acc57a4d30dd54208729388867db7ca9
PramodKumar-SoftwareTester <pramodkumarhl.03@gmail.com>
2026-05-22 16:20:52 +0530

    ChromeBrowser and FireFoxBrowser - 5/22/2026

---
+++ src/main/java/pramod_automation_testing/Fire_Fox_Browser.java
@@ -0,0 +1,31 @@
+package pramod_automation_testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Fire_Fox_Browser {
+    public static void main(String[] args) {
+
+        //Fire Fox Browser
+        //geckodriver
+
+        //System.setProperty("webdriver.gecko.driver","/Users/pramo/Documents/geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://rahulshettyacademy.com/");// URL opened in Fire Fox Browser
+
+        System.out.println(driver.getTitle());//Showing the title of the web page in Output
+
+        System.out.println(driver.getCurrentUrl());// validated the URL
+
+        driver.close();//closing the perticular one browser window
+
+        driver.quit();// close all assosiated browser windows wrt opened by selenium while automation
+
+
+
+
+
+    }
+}
---
+++ src/main/java/pramod_automation_testing/Web_Driver_Invoke.java
@@ -0,0 +1,43 @@
+package pramod_automation_testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Web_Driver_Invoke {
+    public static void main(String[] args) {
+
+        //Invoking Browser
+        //Chrome Browser -> ChromeDriver -> Methods, Get, Close exmpls
+        //FireFox Browser -> Firefox Driver -> Methods - get, Close examples
+        // Safari--- also
+
+
+        //ChromeDriver driver= new ChromeDriver(); -> this Object usable only for
+        //---Chrome driver Methods usage, it will not effect for Fire Fox or any other Browsers
+
+        // webdriver - Methods
+        //Webdriver + Own Class Menthods -> Chrome Driver
+
+        //ChromeDriver.exe --> Chrome Browser
+        // to invoke Chrome Browser There are 2 steps
+        //1 -> Selenium Manager
+
+
+        //2  -> Steps to invoke Chromedriver.exe
+        //System.setProperty("webdriver.chrome.driver","/Users/pramo/Documents/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.flipkart.com/");//Flipkart Web Site Opened
+
+        System.out.println(driver.getTitle());// to get the Title of the Web page in Below Output Console
+        //Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers! - title of the webpage
+
+        System.out.println(driver.getCurrentUrl()); // Validate the URL is it correct or not
+
+        driver.close();// to close the Opened particular Browser window
+
+        driver.quit();// it closes all assosiated Browser windows wrt opened by selenium when doing automation
+
+
+
+    }
+}
